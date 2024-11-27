a8702258f66fb61dc86fa900b0e59c7ff20b2b12
Envyu5 <savin.simon@gmail.com>
2024-11-27 16:46:06 +0100

    Exercise_2

--- SE_01_Exercise_1.py
+++ SE_01_Exercise_1.py
@@ -5,8 +5,6 @@ gameBoard = [['X', 'X', 'X', 'X'], ['X', 'X', 'X', 'X'],['X', 'X', 'X', 'X'],['X
 rows = 12
 cols = 4
 
-anwser = [4, 5, 2, 1]
-
 def Board():
     for x in range(rows):
         print("+---+---+---+---+") 
@@ -43,8 +41,10 @@ def Array(input_count, color_sel):
     elif (turn == 10):
         gameBoard[10][input_count] = color_sel
 
+
 while turn != 13:
     Board()
+    anwser = [4, 5, 2, 1]
     input_count = 0
     for times in range(4):
         color_sel = int(input("\n Choose a color: "))
@@ -55,18 +55,33 @@ while turn != 13:
     if input_count == 4:
         input_count = 0
 
-    hint = 0
-    if gameBoard[turn][0] == anwser[0]:
-        hint += 1
-    if gameBoard[turn][1] == anwser[1]:
-        hint += 1
-    if gameBoard[turn][2] == anwser[2]:
-        hint += 1
-    if gameBoard[turn][3] == anwser[3]:
-        hint += 1
 
-    if hint == 4:
+
+    hint_cor = 0
+    hint_in = 0
+    rowIn = 0
+
+    for i in range(4):
+        if gameBoard[turn][rowIn] == anwser[rowIn]:
+            anwser[rowIn] = 0
+            hint_cor += 1
+
+        rowIn += 1
+    
+    rowIn = 0
+    for b in range(4):
+        if gameBoard[turn][rowIn] in anwser:
+            hint_in += 1
+
+        rowIn += 1
+
+    if hint_cor == 4:
         print('\nYou guessed all pins correctly, You Win!')
-    else:
-        print('\nHint: You guessed ' + str(hint) + ' pins correctly!')
+    elif hint_cor >= 1 and hint_cor < 4 and hint_in == 0:
+        print('\nHint: You guessed ' + str(hint_cor) + ' pins correctly!')
+    elif hint_cor == 0 and hint_in >= 1:
+        print('\nHint: You guessed ' + str(hint_in) + ' but not in the right position.')
+    elif hint_cor >= 1 and hint_cor < 4 and hint_in >= 1:
+        print('\nHint: You guessed ' + str(hint_cor) + ' pins correctly, and ' + str(hint_in) + ' pins that are not in the correct position.' )
+
     turn += 1
